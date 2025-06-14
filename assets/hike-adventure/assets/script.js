a35809a1be4ddd3d1e7f257ef77d8c56af8322b3
1.9.8.2.6 (AJM) <74386189+ajm19826@users.noreply.github.com>
2025-06-14 19:38:23 -0400

    initial upload

---
+++ assets/hike-adventure/assets/script.js
@@ -0,0 +1,114 @@
+/**
+ Assets for Hiking Adventure
+ By SafeLibrary - Alex Manochio 2025
+ */
+function startGame(){
+    window.location.href = "page-1-landing.html";
+}
+function goeastpage1(){
+    window.location.href = "go-east-option-page-1-landing.html";
+}
+function gowestpage1(){
+    window.location.href = "go-west-option-page-1-landing.html";
+}
+function goinsideofhousepage1landing(){
+    window.location.href = "go-inside-of-shed-page-1-landing.html";
+}
+function helpthebikerpage2(){
+    window.location.href = "help-the-biker-page-2.html";
+}
+function gobackpage1landing(){
+    window.location.href = "page-1-landing.html";
+}
+function reset(){
+        window.location.href = "index.html";
+
+}
+function runfrombikerpage2(){
+    window.location.href = "run-from-biker-page-2.html";
+}
+
+function followbikermap(){
+    window.location.href = "follow-biker-map.html";
+
+}
+function goinsidesafelibraryhq(){
+    window.location.href = "go-inside-safelibrary-hq.html";
+
+}
+function gobacktobiker(){
+    window.location.href = "dead-go-back-to-biker.html";
+
+}
+function intrudesafelibrarymeeting(){
+    window.location.href = "intrude-safelibrary-hq-meeting.html";
+
+}
+function hideinbathroomsafelibraryhq(){
+    window.location.href = "hide-in-bathroom-safe-library-hq.html";
+
+}
+function findauniformsafelibrary(){
+    window.location.href = "find-uniform-meeting-room.html";
+
+}
+function runfrommeetingroom(){
+    window.location.href = "run-from-meeting-room-safelibrary.html";
+
+}
+function makefriendsmeetingroom(){
+    window.location.href = "make-some-friends-conference-hall-lunch-time.html";
+
+}
+function begforfriends(){
+    window.location.href = "beg-for-friends.html";
+
+}
+function regretlifechoices(){
+    window.location.href = "regret-life-choices-safelibrary.html";
+
+}
+function haveachat(){
+    window.location.href = "have-a-chat-with-new-hire.html";
+
+}
+function sharelunches(){
+    window.location.href = "share-lunches-with-new-hire.html";
+
+}
+function leavethenewhire(){
+    window.location.href = "leave-the-new-hire.html";
+
+}
+function ignorethenewhiresmess(){
+    window.location.href = "ignore-new-hires-mess.html";
+
+}
+function gotobackofcampus(){
+    window.location.href = "go-to-back-of-campus-new-hire.html";
+
+}
+function findwhatsinbush(){
+    window.location.href = "find-whats-in-bush-campus.html";
+
+}
+function ignorethebush(){
+    window.location.href = "ignore-bush-near-campus.html";
+
+}
+function helpbikercrashbush(){
+    window.location.href = "help-biker-crash-bush-safelibrary-hq.html";
+
+}
+function pickpocketbiker(){
+    window.location.href = "pickpocket-biker-bush.html";
+
+}
+function reportbikertopolice(){
+    window.location.href = "report-biker-to-police.html";
+
+}
+function stealbikerinventory(){
+    window.location.href = "steal-biker-inventory.html";
+
+}
